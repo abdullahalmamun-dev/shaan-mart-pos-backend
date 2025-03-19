55f7642ccd5670c0aebd552421c9de141109d603
hummerheads <mdabdullah.allinfozone@gmail.com>
2025-03-20 00:52:08 +0600

    ready for production

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xgyce0q.mongodb.net/pos-server?retryWrites=true&w=majority`);
+        const conn = await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ebsbi.mongodb.net/pos-server?retryWrites=true&w=majority`);
         console.log(`MongoDB connected successfully to database: ${conn.connection.name}`);
     
     } catch (error) {
